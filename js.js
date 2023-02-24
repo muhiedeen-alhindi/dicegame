81ebec8d756ab05400c77b8c3f2d6d897d6ace33
muhiedeen <muhiedeen.alhindi@gmail.com>
2023-02-24 22:56:11 +0200

    made random  2 dice number

---
+++ js.js
@@ -0,0 +1,14 @@
+let score= document.getElementById("score")
+let dice1=document.getElementById("dice1")
+let dice2=document.getElementById("dice2")
+ 
+ const  first_randomnumber=Math.floor(Math.random() * 6) + 1;
+ const  second_randomnumber=Math.floor(Math.random() * 6) + 1;
+
+
+let img1_src= "resources/dice"+first_randomnumber+".png"
+  
+let img2_src= "resources/dice"+second_randomnumber+".png"
+  
+ dice1.setAttribute("src",img1_src)
+ dice2.setAttribute("src",img2_src)
